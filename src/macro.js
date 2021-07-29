c9eeeaa87a401cd370bea30c89fe69a39200c5c7
renovirTony <renovirtony@gmail.com>
2021-07-29 21:12:52 +0800

    fix event card function

--- src/macro.js
+++ src/macro.js
@@ -455,17 +455,17 @@ function testProjectCards() {
 //draw a new event card
 function drawEventCard() {
   // get current event card from table
-  const currentEventCard = mainBoard.getRange('G20').getDisplayValue();
+  const currentEventCard = mainBoard.getRange('H20').getDisplayValue();
   if (currentEventCard) {
     // remove current event card from table
-    mainBoard.getRange('G20').clearContent();
+    mainBoard.getRange('H20').clearContent();
     // discard current event card to deck
     EventDeck.discard([currentEventCard]);
   }
   // draw event card from deck
   const [newEventCard] = EventDeck.draw();
   // play event card on table
-  mainBoard.getRange('G20').setValue(newEventCard);
+  mainBoard.getRange('H20').setValue(newEventCard);
   SpreadsheetApp.getActive().toast("已翻開新的事件卡");
 }
 
@@ -473,7 +473,7 @@ function drawEventCard() {
 function peekNextEventCard() {
   // open source tree is level 1
   if (mainBoard.getRange('D11').getValue() > 0) {
-    mainBoard.getRange('G21').setValue(spreadsheet.getSheetByName('EventDeck').getRange('A1').getDisplayValue());
+    mainBoard.getRange('H21').setValue(spreadsheet.getSheetByName('EventDeck').getRange('A1').getDisplayValue());
   }
 }
 
