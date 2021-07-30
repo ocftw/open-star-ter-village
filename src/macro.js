7d50436ba3cf3dae2eb45d23266320458ab69229
Ben Liu <ben196888@gmail.com>
2021-07-30 17:06:54 +0700

    feat(macro): add util withCache helper function on getCardSpec

--- src/macro.js
+++ src/macro.js
@@ -21,6 +21,19 @@ function onOpen() {
     .addToUi();
 }
 
+// Utils function
+const withCache = (func, key) => (...args) => {
+  const k = key ? key : `${func.name}-${JSON.stringify(args)}`;
+  if (CacheService.getScriptCache().get(k) !== null) {
+    Logger.log(`get cache from key ${k}`);
+    return JSON.parse(CacheService.getScriptCache().get(k));
+  }
+  Logger.log(`set cache on key ${k}`);
+  const result = func(...args);
+  CacheService.getScriptCache().put(k, JSON.stringify(result));
+  return result;
+};
+
 /**
  * Game cycle design inspected by react component life cycle
  * Game cycle
@@ -225,7 +238,7 @@ const ProjectCardRef = (() => {
   };
 
   return {
-    getSpecByCard,
+    getSpecByCard: withCache(getSpecByCard),
   };
 })();
 
