429227297b61128222ad1056106a061e29e3edf1
huskpurin <huskpurin@gmail.com>
2022-01-26 21:16:55 +0800

    feat(server): handle client side pages requests

--- webapp/server/src/index.ts
+++ webapp/server/src/index.ts
@@ -1,21 +1,38 @@
-import { OpenStarTerVillage } from '@open-star-ter-village/webapp-game';
-import { Server, Origins } from 'boardgame.io/server';
+import { OpenStarTerVillage } from "@open-star-ter-village/webapp-game";
+import path from "path";
+import serve from "koa-static";
+import { Server, Origins } from "boardgame.io/server";
 
 const server = Server({
   games: [OpenStarTerVillage],
   origins: [
     // Allow localhost to connect, except when NODE_ENV is 'production'.
-    Origins.LOCALHOST_IN_DEVELOPMENT
-  ]
+    Origins.LOCALHOST_IN_DEVELOPMENT,
+  ],
 });
+
+// Build path relative to the server.js file
+const frontEndAppBuildPath = path.resolve(__dirname, "./client");
+server.app.use(serve(frontEndAppBuildPath));
 
 const lobbyConfig = {
   apiPort: 8080,
-  apiCallback: () => console.log('Lobby api running on port 8080'),
+  apiCallback: () => console.log("Lobby api running on port 8080"),
 };
 
-server.run({
-  port: 8000,
-  lobbyConfig,
-  callback: () => console.log('Main server running on port 8000...'),
-});
+server.run(
+  {
+    port: 8000,
+    lobbyConfig,
+    callback: () => console.log("Main server running on port 8000..."),
+  },
+  () => {
+    server.app.use(
+      async (ctx, next) =>
+        await serve(frontEndAppBuildPath)(
+          Object.assign(ctx, { path: "index.html" }),
+          next
+        )
+    );
+  }
+);
