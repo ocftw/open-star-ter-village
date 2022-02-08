9d26b3c351a3bb97aaa36af7326a7b8f06bdf27d
Ben Liu <ben196888@gmail.com>
2022-02-09 04:47:54 +0700

    feat(webapp-client): add move buttons to trigger moves

--- webapp/client/src/CurrentPlayer/CurrentPlayer.tsx
+++ webapp/client/src/CurrentPlayer/CurrentPlayer.tsx
@@ -2,13 +2,27 @@ import { BoardProps } from 'boardgame.io/react';
 import { OpenStarTerVillageType } from 'packages/game/src/types';
 
 const CurrentPlayer: React.FC<BoardProps<OpenStarTerVillageType.State.Root>> = (props) => {
-  const { G, playerID } = props;
+  const { G, playerID, moves, events } = props;
   if (playerID === null) {
     return null;
   }
+  // TODO: replace indices with selected states
+  const projectCardIndex: number = 0;
+  const resourceCardIndex: number = 0;
+  const activeProjectIndex: number = 0;
+  const onCreateProject = () => moves.createProject(projectCardIndex, resourceCardIndex);
+  const onRecruit = () => moves.recruit(resourceCardIndex, activeProjectIndex);
+  const onEndAction = () => events.endStage!();
+  const onRefillAndEnd = () => moves.refillAndEnd();
   return (
     <div>
       <div>I am Player {playerID}</div>
+      <div>
+        <button onClick={onCreateProject}>Create Project</button>
+        <button onClick={onRecruit}>Recruit</button>
+        <button onClick={onEndAction}>End Action</button>
+        <button onClick={onRefillAndEnd}>Refill and End</button>
+      </div>
       <div>
         Project Cards:
         <ul>
