d34317b96f5ed44ba0c21e3c5fce6afc52f40ffe
Ben Liu <ben196888@gmail.com>
2022-04-08 08:41:19 +0700

    refactor(webapp-game): move off mock data from each test cases

--- packages/game/src/activeProjects.test.ts
+++ packages/game/src/activeProjects.test.ts
@@ -1,12 +1,16 @@
 import { ActiveProject, ActiveProjects } from "./activeProjects";
 import { OpenStarTerVillageType } from "./types";
 
+const mockCard: OpenStarTerVillageType.Card.Project = { name: 'test 1', jobs: ['a', 'a', 'a', 'b', 'c', 'c'] };
+const mockPlayer = 'test owner';
+
 describe('ActiveProjects', () => {
   describe('Add', () => {
     it('should append a new project', () => {
       const activeProjects: OpenStarTerVillageType.State.Project[] = [];
-      const card: OpenStarTerVillageType.Card.Project = { name: 'test 1', jobs: ['a', 'a', 'a', 'b', 'c', 'c'] };
-      const activeProjectId = ActiveProjects.Add(activeProjects, card, 'test owner');
+
+      const activeProjectId = ActiveProjects.Add(activeProjects, mockCard, mockPlayer);
+
       expect(activeProjectId).toEqual(0);
     });
   });
@@ -14,10 +18,11 @@ describe('ActiveProjects', () => {
   describe('GetById', () => {
     it('should return active project', () => {
       const activeProjects: OpenStarTerVillageType.State.Project[] = [];
-      const card: OpenStarTerVillageType.Card.Project = { name: 'test 1', jobs: ['a', 'a', 'a', 'b', 'c', 'c'] };
-      const activeProjectId = ActiveProjects.Add(activeProjects, card, 'test owner');
+      const activeProjectId = ActiveProjects.Add(activeProjects, mockCard, mockPlayer);
+
       const activeProject = ActiveProjects.GetById(activeProjects, activeProjectId);
-      expect(activeProject.card).toEqual(card);
+
+      expect(activeProject.card).toEqual(mockCard);
     });
   });
 });
@@ -26,20 +31,19 @@ describe('ActiveProject', () => {
   describe('AssignWorker', () => {
     it('should recored player name in worker slot', () => {
       const activeProjects: OpenStarTerVillageType.State.Project[] = [];
-      const card: OpenStarTerVillageType.Card.Project = { name: 'test 1', jobs: ['a', 'a', 'a', 'b', 'c', 'c'] };
-      const activeProjectId = ActiveProjects.Add(activeProjects, card, 'test owner');
+      const activeProjectId = ActiveProjects.Add(activeProjects, mockCard, mockPlayer);
       const activeProject = ActiveProjects.GetById(activeProjects, activeProjectId);
 
-      ActiveProject.AssignWorker(activeProject, 3, 'test owner');
-      expect(activeProject.workers[3]).toEqual('test owner');
+      ActiveProject.AssignWorker(activeProject, 3, mockPlayer);
+
+      expect(activeProject.workers[3]).toEqual(mockPlayer);
     });
   });
 
   describe('Contribute', () => {
     it('should increase value by slot and by job', () => {
       const activeProjects: OpenStarTerVillageType.State.Project[] = [];
-      const card: OpenStarTerVillageType.Card.Project = { name: 'test 1', jobs: ['a', 'a', 'a', 'b', 'c', 'c'] };
-      const activeProjectId = ActiveProjects.Add(activeProjects, card, 'test owner');
+      const activeProjectId = ActiveProjects.Add(activeProjects, mockCard, mockPlayer);
       const activeProject = ActiveProjects.GetById(activeProjects, activeProjectId);
 
       ActiveProject.Contribute(activeProject, 0, 3);
