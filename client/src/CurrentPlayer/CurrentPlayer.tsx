cdedecb5231e04aadcb85b49fcf470dd8c23589e
Ben Liu <ben196888@gmail.com>
2022-04-10 23:39:26 +0700

    feat(webapp-client): add settle move button

--- client/src/CurrentPlayer/CurrentPlayer.tsx
+++ client/src/CurrentPlayer/CurrentPlayer.tsx
@@ -42,6 +42,7 @@ const CurrentPlayer: React.FC<BoardProps<Type.State.Root>> = (props) => {
     setContributions([]);
   };
   const onEndAction = () => events.endStage!();
+  const onSettle = () => moves.settle();
   const onRefillAndEnd = () => moves.refillAndEnd();
   const myCurrentStage = ctx.activePlayers ? ctx.activePlayers[playerID] : ''
   return (
@@ -105,6 +106,7 @@ const CurrentPlayer: React.FC<BoardProps<Type.State.Root>> = (props) => {
         <div>current contribution entities: {JSON.stringify(contributions)}</div>
         <button onClick={onContribute}>Contribute</button>
         <button onClick={onEndAction}>End Action</button>
+        <button onClick={onSettle}>Settle</button>
         <button onClick={onRefillAndEnd}>Refill and End</button>
       </div>
       <div>
