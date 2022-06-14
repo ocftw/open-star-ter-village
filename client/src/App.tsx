2dd88b3fe71a9fba576aa49eb3c95185a0a7978c
huskpurin <huskpurin@gmail.com>
2022-06-14 23:53:00 +0800

    feat(webapp-client): add board button on Header to switch player view

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,22 @@
-import { Heading, HStack, Box, Text, Link, Image } from '@chakra-ui/react';
+import { HStack, Box, Text, Link, Image, Button, ButtonGroup } from '@chakra-ui/react';
 import Boardgame from './BoardGame';
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+const BOARDS = [
+  { id: 'overview', playerId: undefined, title: 'Overview' },
+  { id: '0', playerId: '0', title: 'Player 0' },
+  { id: '1', playerId: '1', title: 'Player 1' },
+];
+
 
 function App() {
+  const [currentBoard, setCurrentBoard] = useState(BOARDS[1]);
+
   return (
     <>
-      <HStack bg="blue.900" align="center" justify="center">
+      <HStack bg="blue.900" align="center">
         <Image src={logo} alt="logo" boxSize="62px" className="App-logo" />
         <Box py="4">
           <Text color="white" fontSize="xl">OpenStarTerVillage</Text>
@@ -21,18 +31,22 @@ function App() {
             Clone from github
           </Link>
         </Box>
+        <Box py="4" pl="2">
+          <ButtonGroup size="xs">
+            {BOARDS.map((board => (
+              <Button
+                key={board.id}
+                isActive={board === currentBoard}
+                onClick={() => setCurrentBoard(board)}
+              >
+                {board.title}
+              </Button>
+            )))}
+          </ButtonGroup>
+        </Box>
       </HStack>
-      <Box ml="10" mt="5">
-        <Heading as="h1" size="xl">Player 0 view</Heading>
-        <Boardgame playerID="0" />
-      </Box>
-      <Box ml="10" mt="5">
-        <Heading as="h1" size="xl">Player 1 view</Heading>
-        <Boardgame playerID="1" />
-      </Box>
-      <Box ml="10" mt="5">
-        <Heading as="h1" size="xl">Observer view</Heading>
-        <Boardgame />
+      <Box my="5">
+        <Boardgame playerID={currentBoard.playerId} />
       </Box>
     </>
   );
