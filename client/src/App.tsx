50e30a910cc7c53f92a0726e89b769644fc02ccd
huskpurin <huskpurin@gmail.com>
2022-07-06 09:10:22 +0800

    refactor(webapp-client): separate Header feature from App

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,52 +1,18 @@
-import { HStack, Box, Text, Link, Image, Button, ButtonGroup } from '@chakra-ui/react';
+import { Box } from '@chakra-ui/react';
 import Boardgame from './BoardGame';
-import logo from './logo.svg';
 import './App.css';
+import Header from './features/Header/Header';
 import { useState } from 'react';
-
-const BOARDS = [
-  { id: 'overview', playerId: undefined, title: 'Overview' },
-  { id: '0', playerId: '0', title: 'Player 0' },
-  { id: '1', playerId: '1', title: 'Player 1' },
-];
 
 
 function App() {
-  const [currentBoard, setCurrentBoard] = useState(BOARDS[1]);
+  const [playerId, setPlayerId] = useState<string | undefined>();
 
   return (
     <>
-      <HStack bg="blue.900" align="center">
-        <Image src={logo} alt="logo" boxSize="62px" className="App-logo" />
-        <Box py="4">
-          <Text color="white" fontSize="xl">OpenStarTerVillage</Text>
-        </Box>
-        <Box py="4" pl="2">
-          <Link
-            color="cyan.300"
-            href="https://github.com/ocftw/open-star-ter-village"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Clone from github
-          </Link>
-        </Box>
-        <Box py="4" pl="2">
-          <ButtonGroup size="xs">
-            {BOARDS.map((board => (
-              <Button
-                key={board.id}
-                isActive={board === currentBoard}
-                onClick={() => setCurrentBoard(board)}
-              >
-                {board.title}
-              </Button>
-            )))}
-          </ButtonGroup>
-        </Box>
-      </HStack>
+      <Header playerId={playerId} onPlayerIdChange={setPlayerId} />
       <Box my="5">
-        <Boardgame playerID={currentBoard.playerId} />
+        <Boardgame playerID={playerId} />
       </Box>
     </>
   );
---
+++ client/src/features/Header/Header.tsx
@@ -0,0 +1,60 @@
+import { HStack, Box, Text, Link, Image, Button, ButtonGroup } from '@chakra-ui/react';
+import logo from './logo.svg';
+import { useMemo, useCallback } from 'react';
+
+const BOARDS = [
+  { id: 'overview', playerId: undefined, title: 'Overview' },
+  { id: '0', playerId: '0', title: 'Player 0' },
+  { id: '1', playerId: '1', title: 'Player 1' },
+];
+
+interface IHeaderProps {
+  playerId?: string;
+  onPlayerIdChange: (playerId: IHeaderProps['playerId']) => void;
+}
+
+const Header: React.FunctionComponent<IHeaderProps> = ({ playerId, onPlayerIdChange }) => {
+  const currentBoard = useMemo(() => {
+    return BOARDS.find(b => b.playerId === playerId);
+  }, [playerId]);
+
+  const handleBoardButtonClick: React.MouseEventHandler<HTMLButtonElement> = useCallback((e) => {
+    const playerId = e.currentTarget.getAttribute('data-player-id') || undefined;
+    onPlayerIdChange(playerId);
+  }, [onPlayerIdChange]);
+
+  return (
+    <HStack bg="blue.900" align="center">
+      <Image src={logo} alt="logo" boxSize="62px" className="App-logo" />
+      <Box py="4">
+        <Text color="white" fontSize="xl">OpenStarTerVillage</Text>
+      </Box>
+      <Box py="4" pl="2">
+        <Link
+          color="cyan.300"
+          href="https://github.com/ocftw/open-star-ter-village"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Clone from github
+        </Link>
+      </Box>
+      <Box py="4" pl="2">
+        <ButtonGroup size="xs">
+          {BOARDS.map((board => (
+            <Button
+              key={board.id}
+              data-player-id={board.playerId}
+              isActive={board === currentBoard}
+              onClick={handleBoardButtonClick}
+            >
+              {board.title}
+            </Button>
+          )))}
+        </ButtonGroup>
+      </Box>
+    </HStack>
+  );
+};
+
+export default Header;
