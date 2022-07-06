b4f397c65556b2c40b8d2b4d5ad3a4c53f1cf9f4
Ben Liu <ben196888@gmail.com>
2022-07-07 00:26:29 +0700

    feat(webapp-client): polish current player debug page

--- client/src/CurrentPlayer/CurrentPlayer.tsx
+++ client/src/CurrentPlayer/CurrentPlayer.tsx
@@ -13,15 +13,17 @@ import {
   NumberInputStepper,
   NumberIncrementStepper,
   NumberDecrementStepper,
-  Divider,
-  Center,
+  Tabs,
+  TabList,
+  Tab,
+  TabPanel,
+  TabPanels,
 } from '@chakra-ui/react'
 
 const CurrentPlayer: React.FC<BoardProps<Type.State.Root>> = (props) => {
   const { G, playerID, moves: nonTypeMoves, events, ctx } = props;
   const moves = nonTypeMoves as unknown as Type.Move.Moves;
 
-  // TODO: replace indices with selected states
   const [projectCardIndex, setProjectCardIndex] = useState(0);
   const [activeJobCardIndex, setActiveJobCardIndex] = useState(0);
   const [activeProjectIndex, setActiveProjectIndex] = useState(0);
@@ -51,88 +53,133 @@ const CurrentPlayer: React.FC<BoardProps<Type.State.Root>> = (props) => {
     setContributions([]);
   };
   const onEndAction = () => events.endStage!();
-  const onSettle = () => moves.settle();
   const onEndSettle = () => events.endStage!();
   const onRefillAndEnd = () => moves.refillAndEnd();
   const myCurrentStage = ctx.activePlayers ? ctx.activePlayers[playerID] : ''
+
+  const renderHandProjectCards = () => (
+    <HStack w={['100%', '100%', '20%']}>
+      <Box as='label'>Hand project cards</Box>
+      <RadioGroup onChange={val => setProjectCardIndex(parseInt(val))} value={projectCardIndex}>
+        {
+          G.players[playerID].hand.projects.map((project, index) =>
+            <Radio key={`${project.name}-${index}`} value={index}>{project.name} <br /> {JSON.stringify(project.requirements)}</Radio>)
+        }
+      </RadioGroup>
+    </HStack>
+  );
+
+  const renderActiveJobCards = () => (
+    <HStack w={['100%', '100%', '20%']}>
+      <Box as='label'>Active job cards</Box>
+      <RadioGroup onChange={val => setActiveJobCardIndex(parseInt(val))} value={activeJobCardIndex}>
+        {
+          G.table.activeJobs.map((job, index) =>
+            <Radio key={`${job.name}-${index}`} value={index}>{job.name}</Radio>)
+        }
+      </RadioGroup>
+    </HStack>
+  );
+
+  const renderActiveProjectCards = () => (
+    <HStack w={['100%', '100%', '20%']}>
+      <Box as='label'>Active project cards</Box>
+      <RadioGroup onChange={val => setActiveProjectIndex(parseInt(val))} value={activeProjectIndex}>
+        {
+          G.table.activeProjects.map(activeProject => activeProject.card).map((project, index) =>
+            <Radio key={`${project.name}-${index}`} value={index}>{project.name} <br /> {JSON.stringify(project.requirements)}</Radio>)
+        }
+      </RadioGroup>
+    </HStack>
+  );
+
+  const renderActiveProjectJobName = () => (
+    <HStack w={['100%', '100%', '20%']}>
+      <Box as='label'>Job name</Box>
+      <RadioGroup onChange={setJobName} value={jobName}>
+        {
+          Object.keys(G.table.activeProjects[activeProjectIndex]?.card.requirements ?? []).map((jobName) =>
+            <Radio key={`${activeProjectIndex}-${jobName}`} value={jobName}>{jobName}</Radio>)
+        }
+      </RadioGroup>
+    </HStack>
+  );
+
+  const renderValueInputBox = () => (
+    <HStack w={['100%', '100%', '20%']}>
+      <Box as='label'>Value</Box>
+      <NumberInput min={0} max={5} value={value} onChange={(_, val) => setValue(val)}>
+        <NumberInputField />
+        <NumberInputStepper>
+          <NumberIncrementStepper />
+          <NumberDecrementStepper />
+        </NumberInputStepper>
+      </NumberInput>
+    </HStack>
+  );
+
   return (
     <div className='CurrentPlayer'>
-      <div>I am Player {playerID}</div>
       {myCurrentStage ? <div>my current stage: {myCurrentStage}</div> : null}
-      <Stack direction={['column', 'row']} mt={2}>
-        <HStack w={['100%', '100%', '20%']}>
-          <Box as='label'>Hand project cards</Box>
-          <RadioGroup onChange={val => setProjectCardIndex(parseInt(val))} value={projectCardIndex}>
-            {
-              G.players[playerID].hand.projects.map((project, index) =>
-                <Radio key={`${project.name}-${index}`} value={index}>{project.name} <br /> {JSON.stringify(project.requirements)}</Radio>)
-            }
-          </RadioGroup>
-        </HStack>
-      </Stack>
-      <Stack direction={['column', 'row']} mt={2}>
-        <HStack w={['100%', '100%', '20%']}>
-          <Box as='label'>Active job cards</Box>
-          <RadioGroup onChange={val => setActiveJobCardIndex(parseInt(val))} value={activeJobCardIndex}>
-            {
-              G.table.activeJobs.map((job, index) =>
-                <Radio key={`${job.name}-${index}`} value={index}>{job.name}</Radio>)
-            }
-          </RadioGroup>
-        </HStack>
-      </Stack>
-      <Stack direction={['column', 'row']} mt={2}>
-        <HStack w={['100%', '100%', '20%']}>
-          <Box as='label'>Active project cards</Box>
-          <RadioGroup onChange={val => setActiveProjectIndex(parseInt(val))} value={activeProjectIndex}>
-            {
-              G.table.activeProjects.map((project, index) =>
-                <Radio key={`${project.card.name}-${index}`} value={index}>{project.card.name}</Radio>)
-            }
-          </RadioGroup>
-        </HStack>
-        <Center height='50px'>
-          <Divider orientation='vertical' />
-        </Center>
-        <HStack w={['100%', '100%', '20%']}>
-          <Box as='label'>Job name</Box>
-          <RadioGroup onChange={setJobName} value={jobName}>
-            {
-              Object.keys(G.table.activeProjects[activeProjectIndex]?.card.requirements ?? []).map((jobName) =>
-                <Radio key={`${activeProjectIndex}-${jobName}`} value={jobName}>{jobName}</Radio>)
-            }
-          </RadioGroup>
-        </HStack>
-        <Center height='50px'>
-          <Divider orientation='vertical' />
-        </Center>
-        <HStack w={['100%', '100%', '20%']}>
-          <Box as='label'>Value</Box>
-          <NumberInput min={0} max={5} value={value} onChange={(_, val) => setValue(val)}>
-            <NumberInputField />
-            <NumberInputStepper>
-              <NumberIncrementStepper />
-              <NumberDecrementStepper />
-            </NumberInputStepper>
-          </NumberInput>
-        </HStack>
-      </Stack>
       {
         myCurrentStage === 'action' &&
         <>
-          <Stack direction={['column', 'row']} mt={1}>
-            <Button size='xs' onClick={onCreateProject}>Create Project</Button>
-            <Button size='xs' onClick={onRecruit}>Recruit</Button>
-            <Button size='xs' onClick={onAddContribution}>add contribution entity</Button>
-          </Stack>
-          <Stack direction={['column', 'row']} mt={1}>
-            <div>current contribution entities: {JSON.stringify(contributions)}</div>
-          </Stack>
-          <Stack direction={['column', 'row']} mt={1}>
-            <Button size='xs' onClick={onContributeOwnedProjects}>ContributeOwnedProjects</Button>
-            <Button size='xs' onClick={onContributeJoinedProjects}>ContributeJoinedProjects</Button>
-            <Button size='xs' onClick={onEndAction}>End Action</Button>
-          </Stack>
+          <Tabs>
+            <TabList>
+              <Tab>Create Project</Tab>
+              <Tab>Recruit</Tab>
+              <Tab>Contribute Owned Projects</Tab>
+              <Tab>Contribute Joined Projects</Tab>
+              <Tab>Remove and Refill Jobs</Tab>
+              <Tab>Mirror</Tab>
+              <Button color='red' onClick={onEndAction}>End Action</Button>
+            </TabList>
+            <TabPanels>
+              <TabPanel>
+                <Stack direction={['column', 'row']} mt={2}>
+                  {renderHandProjectCards()}
+                  {renderActiveJobCards()}
+                </Stack>
+                <Button onClick={onCreateProject}>Create Project</Button>
+              </TabPanel>
+              <TabPanel>
+                <Stack direction={['column', 'row']} mt={2}>
+                  {renderActiveProjectCards()}
+                  {renderActiveJobCards()}
+                </Stack>
+                <Button onClick={onRecruit}>Recruit</Button>
+              </TabPanel>
+              <TabPanel>
+                <Stack direction={['column', 'row']} mt={2}>
+                  {renderActiveProjectCards()}
+                  {renderActiveProjectJobName()}
+                  {renderValueInputBox()}
+                  <Button size='xs' onClick={onAddContribution}>add contribution entity</Button>
+                  <Box>current contribution entities: {JSON.stringify(contributions)}</Box>
+                </Stack>
+                <Button onClick={onContributeOwnedProjects}>ContributeOwnedProjects</Button>
+              </TabPanel>
+              <TabPanel>
+                <Stack direction={['column', 'row']} mt={2}>
+                  {renderActiveProjectCards()}
+                  {renderActiveProjectJobName()}
+                  {renderValueInputBox()}
+                  <Button size='xs' onClick={onAddContribution}>add contribution entity</Button>
+                  <Box>current contribution entities: {JSON.stringify(contributions)}</Box>
+                </Stack>
+                <Button onClick={onContributeJoinedProjects}>ContributeJoinedProjects</Button>
+              </TabPanel>
+              <TabPanel>
+                <Stack direction={['column', 'row']} mt={2}>
+                  {renderActiveJobCards()}
+                </Stack>
+                <Button >Remove and Refill Jobs</Button>
+              </TabPanel>
+              <TabPanel>
+                <Button >Mirror</Button>
+              </TabPanel>
+            </TabPanels>
+          </Tabs>
         </>
       }
       {
@@ -140,8 +187,7 @@ const CurrentPlayer: React.FC<BoardProps<Type.State.Root>> = (props) => {
         <>
           <Stack direction={['column', 'row']} mt={1}>
             <div className='group settles'>
-              <Button size='xs' onClick={onSettle}>Settle</Button>
-              <Button size='xs' onClick={onEndSettle}>End Settle</Button>
+              <Button color='red' onClick={onEndSettle}>End Settle</Button>
             </div>
           </Stack>
         </>
@@ -151,7 +197,7 @@ const CurrentPlayer: React.FC<BoardProps<Type.State.Root>> = (props) => {
         <>
           <Stack direction={['column', 'row']} mt={1}>
             <div className='group discards'>
-              <Button size='xs' onClick={() => events.endStage!()}>End Discard</Button>
+              <Button color='red' onClick={() => events.endStage!()}>End Discard</Button>
             </div>
           </Stack>
         </>
@@ -160,7 +206,7 @@ const CurrentPlayer: React.FC<BoardProps<Type.State.Root>> = (props) => {
         myCurrentStage === 'refill' &&
         <>
           <Stack direction={['column', 'row']} mt={1}>
-            <Button size='xs' onClick={onRefillAndEnd}>Refill and End turn</Button>
+            <Button color='red' onClick={onRefillAndEnd}>Refill and End turn</Button>
           </Stack>
         </>
       }
