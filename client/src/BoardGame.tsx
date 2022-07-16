0b7127a8379e4baa5e5d0f2046283a70a3178fb8
huskpurin <huskpurin@gmail.com>
2022-07-16 19:01:33 +0800

    feat(webapp-client): add ActionBoard with basic style

--- client/src/BoardGame.tsx
+++ client/src/BoardGame.tsx
@@ -3,12 +3,14 @@ import { Client, BoardProps } from 'boardgame.io/react';
 import { Local } from 'boardgame.io/multiplayer';
 import { OpenStarTerVillageType } from 'packages/game/src/types';
 import Table from './features/Table/Table';
+import ActionBoard from './features/ActionBoard/ActionBoard';
 import Players from './Players/Players';
 import CurrentPlayer from './CurrentPlayer/CurrentPlayer';
 
 const Board: React.FC<BoardProps<OpenStarTerVillageType.State.Root>> = (props) => {
   return (
     <div className='Board'>
+      <ActionBoard {...props} />
       <Table {...props} />
       <Players {...props} />
       <CurrentPlayer {...props} />
