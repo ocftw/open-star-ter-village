07f60e78ae8e059b4b557fb883dc2e7dad348843
huskpurin <huskpurin@gmail.com>
2022-07-21 02:10:10 +0800

    feat(webapp-client): init actionBoard player turn

--- client/src/features/ActionBoard/ActionBoard.tsx
+++ client/src/features/ActionBoard/ActionBoard.tsx
@@ -1,11 +1,21 @@
-import * as React from 'react';
+import { useEffect } from 'react';
 import { BoardProps } from 'boardgame.io/react';
 import { OpenStarTerVillageType as Type } from 'packages/game/src/types';
 import { Box, Heading, ButtonGroup, Button } from '@chakra-ui/react'
+import { useAppDispatch } from '../../app/hooks';
+import { playerTurnInited } from './actionBoardSlice';
+
 
 const ActionBoard: React.FC<BoardProps<Type.State.Root>> = (props) => {
+  const dispatch = useAppDispatch();
   const playerID = props.playerID;
   const currentPlayer = props.ctx.currentPlayer;
+
+  useEffect(() => {
+    if (playerID === currentPlayer) {
+      dispatch(playerTurnInited(currentPlayer));
+    }
+  }, [playerID, currentPlayer, dispatch]);
 
   return (
     <>
