22b33919e9007558cac74bff3b87e067fba0c444
huskpurin <huskpurin@gmail.com>
2022-07-21 02:26:40 +0800

    feat(webapp-client): init actionBoard move when click createProjectMove button

--- client/src/features/ActionBoard/ActionBoard.tsx
+++ client/src/features/ActionBoard/ActionBoard.tsx
@@ -1,21 +1,31 @@
-import { useEffect } from 'react';
+import { useCallback, useEffect } from 'react';
 import { BoardProps } from 'boardgame.io/react';
 import { OpenStarTerVillageType as Type } from 'packages/game/src/types';
 import { Box, Heading, ButtonGroup, Button } from '@chakra-ui/react'
-import { useAppDispatch } from '../../app/hooks';
-import { playerTurnInited } from './actionBoardSlice';
-
+import { useAppDispatch, useAppSelector } from '../../app/hooks';
+import { playerTurnInited, moveInited } from './actionBoardSlice';
+import { MoveStatus } from './ActionBoard.types';
 
 const ActionBoard: React.FC<BoardProps<Type.State.Root>> = (props) => {
   const dispatch = useAppDispatch();
   const playerID = props.playerID;
   const currentPlayer = props.ctx.currentPlayer;
+  const createProjectMove = useAppSelector(state => state.actionBoard.moves.find(move => move.move === 'createProject'));
+  const isCreateProjectMoveActive = createProjectMove?.status === MoveStatus.editing;
 
   useEffect(() => {
     if (playerID === currentPlayer) {
       dispatch(playerTurnInited(currentPlayer));
     }
   }, [playerID, currentPlayer, dispatch]);
+
+  const handleCreateProjectButtonClick = useCallback(() => {
+    if (isCreateProjectMoveActive) {
+      return;
+    }
+
+    dispatch(moveInited({ moveType: 'createProject' }));
+  }, [isCreateProjectMoveActive, dispatch]);
 
   return (
     <>
@@ -25,7 +35,12 @@ const ActionBoard: React.FC<BoardProps<Type.State.Root>> = (props) => {
       <Box m="4" p="4" border="1px" borderRadius="lg" borderColor="gray.100">
         {playerID === currentPlayer && (
           <ButtonGroup size='sm' spacing='6'>
-            <Button>Create Project</Button>
+            <Button
+              onClick={handleCreateProjectButtonClick}
+              colorScheme={isCreateProjectMoveActive ? "red" : "gray"}
+            >
+              Create Project
+            </Button>
             <Button disabled>Move 2</Button>
             <Button disabled>Move 3</Button>
           </ButtonGroup>
