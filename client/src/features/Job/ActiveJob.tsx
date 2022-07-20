824dedc4e29a4989886a15682a857c002cdeb1a3
huskpurin <huskpurin@gmail.com>
2022-07-21 03:02:32 +0800

    feat(webapp-client): make ActiveJob togglable

--- client/src/features/Job/ActiveJob.tsx
+++ client/src/features/Job/ActiveJob.tsx
@@ -1,15 +1,23 @@
+import { useCallback } from 'react';
 import { Box, Text } from '@chakra-ui/react';
 import { OpenStarTerVillageType as Type } from 'packages/game/src/types';
 
 export interface IActiveJobProps {
   job: Type.Card.Job;
+  jobIndex: number;
+  onClick: (e: { job: IActiveJobProps['job'], jobIndex: IActiveJobProps['jobIndex'] }) => void;
+  selected?: boolean;
 }
 
 const ActiveJob: React.FC<IActiveJobProps> = (props) => {
-  const { job } = props;
+  const { job, jobIndex, onClick, selected } = props;
+  const boarderColor = selected ? "red.300" : "gray.100";
+  const handleClick = useCallback(() => {
+    onClick({ job, jobIndex });
+  }, [job, jobIndex, onClick])
 
   return (
-    <Box m="4" p="4" border="1px" borderRadius="lg" borderColor="gray.100">
+    <Box m="4" p="4" border="1px" borderRadius="lg" borderColor={boarderColor} onClick={handleClick}>
       <Text fontSize='sm'>{job.name}</Text>
     </Box>
   );
--- client/src/features/Table/Table.tsx
+++ client/src/features/Table/Table.tsx
@@ -1,14 +1,33 @@
+import { useCallback } from 'react';
 import { BoardProps } from 'boardgame.io/react';
 import { OpenStarTerVillageType as Type } from 'packages/game/src/types';
 import { Box, Stack } from '@chakra-ui/react';
 import ActiveProject from '../Project/ActiveProject';
 import ActiveJob from '../Job/ActiveJob';
+import { useAppSelector, useAppDispatch } from '../../app/hooks';
+import { MoveStatus } from '../ActionBoard/ActionBoard.types';
+import { tableJobsToggled } from '../ActionBoard/actionBoardSlice';
 
 type Props = BoardProps<Type.State.Root>;
 
 const Table: React.FC<Props> = (props) => {
+  const dispatch = useAppDispatch();
   const activeProjects = [...props.G.table.activeProjects, ...Array(6)].slice(0, 6);
   const activeJobs = props.G.table.activeJobs;
+  const isCurrentPlayer = props.playerID === props.ctx.currentPlayer;
+  const currentMoveIndex = useAppSelector(state =>
+    state.actionBoard.moves.findIndex(move => move.status === MoveStatus.editing)
+  );
+  const currentMove = useAppSelector(state =>
+    state.actionBoard.moves.find(move => move.status === MoveStatus.editing)
+  );
+  const handleActiveJobClick = useCallback(({ jobIndex }) => {
+    if (!isCurrentPlayer) {
+      return;
+    }
+
+    dispatch(tableJobsToggled({ moveIndex: currentMoveIndex, tableJobsIndex: jobIndex }));
+  }, [isCurrentPlayer, currentMoveIndex, dispatch])
 
   return (
     <Box>
@@ -26,7 +45,12 @@ const Table: React.FC<Props> = (props) => {
           <Stack direction={['column', 'column', 'row']} wrap="wrap" mt={2} spacing={0}>
             {activeJobs.map((job, jobIndex) => (
               <Box key={`active-job-${jobIndex}`} w={['100%', '100%', '100%', '100%', '50%']}>
-                <ActiveJob job={job} />
+                <ActiveJob
+                  job={job}
+                  jobIndex={jobIndex}
+                  selected={currentMove?.selections.tableJobs[jobIndex]}
+                  onClick={handleActiveJobClick}
+                />
               </Box>
             ))}
           </Stack>
