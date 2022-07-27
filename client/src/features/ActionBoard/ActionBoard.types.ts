76e4b9a63087be9eb3e93080ae5a788e28603d4f
huskpurin <huskpurin@gmail.com>
2022-07-27 23:47:32 +0800

    fix(webapp-client): remove useless state

--- client/src/features/ActionBoard/ActionBoard.types.ts
+++ client/src/features/ActionBoard/ActionBoard.types.ts
@@ -2,14 +2,8 @@ import { OpenStarTerVillageType as Type } from 'packages/game/src/types';
 
 export type MoveType = keyof Type.Move.AllMoves;
 
-export enum MoveStatus {
-  'editing',
-  'submitted',
-}
-
 export interface Move {
   move: MoveType;
-  status: MoveStatus;
   selections: {
     tableProject: boolean[];
     tableJobs: boolean[];
--- client/src/features/ActionBoard/actionBoardSlice.tsx
+++ client/src/features/ActionBoard/actionBoardSlice.tsx
@@ -1,7 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit'
 import type { PayloadAction } from '@reduxjs/toolkit'
 import { PlayerID } from 'boardgame.io';
-import { MoveStatus, MoveType, Move } from './ActionBoard.types';
+import { MoveType, Move } from './ActionBoard.types';
 
 export interface ActionBoardState {
   currentPlayer: PlayerID | null;
@@ -28,7 +28,6 @@ export const ActionBoardSlice = createSlice({
 
       state.moves.push({
         move: moveType,
-        status: MoveStatus.editing,
         selections: {
           tableProject: [],
           tableJobs: [],
@@ -40,35 +39,29 @@ export const ActionBoardSlice = createSlice({
     tableProjectToggled: (state, action: PayloadAction<{ moveIndex: number, tableProjectIndex: number }>) => {
       const { moveIndex, tableProjectIndex } = action.payload;
 
-      state.moves[moveIndex].status = MoveStatus.editing;
       state.moves[moveIndex].selections.tableProject[tableProjectIndex] =
         !state.moves[moveIndex].selections.tableProject[tableProjectIndex];
     },
     tableJobsToggled: (state, action: PayloadAction<{ moveIndex: number, tableJobsIndex: number }>) => {
       const { moveIndex, tableJobsIndex } = action.payload;
 
-      state.moves[moveIndex].status = MoveStatus.editing;
       state.moves[moveIndex].selections.tableJobs[tableJobsIndex] =
         !state.moves[moveIndex].selections.tableJobs[tableJobsIndex];
     },
     handProjectsToggled: (state, action: PayloadAction<{ moveIndex: number, handProjectsIndex: number }>) => {
       const { moveIndex, handProjectsIndex } = action.payload;
 
-      state.moves[moveIndex].status = MoveStatus.editing;
       state.moves[moveIndex].selections.handProjects[handProjectsIndex] =
         !state.moves[moveIndex].selections.handProjects[handProjectsIndex];
     },
     handForcesToggled: (state, action: PayloadAction<{ moveIndex: number, handForcesIndex: number }>) => {
       const { moveIndex, handForcesIndex } = action.payload;
 
-      state.moves[moveIndex].status = MoveStatus.editing;
       state.moves[moveIndex].selections.handForces[handForcesIndex] =
         !state.moves[moveIndex].selections.handForces[handForcesIndex];
     },
     moveSubmitted: (state, action: PayloadAction<{ moveIndex: number }>) => {
-      const { moveIndex } = action.payload;
       // TODO: call api
-      state.moves[moveIndex].status = MoveStatus.submitted;
     }
   }
 })
