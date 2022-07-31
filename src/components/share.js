7f1064ac47b25d21f4748f5298790c27e945f1ea
Ben Liu <ben196888@gmail.com>
2022-07-31 19:57:54 +0700

    fix(homePage): remove twitterHandle

--- src/components/share.js
+++ src/components/share.js
@@ -23,7 +23,6 @@ const Share = ({ socialConfig }) => {
         url={socialConfig.config.url}
         className="button is-outlined is-rounded twitter"
         title={socialConfig.config.title}
-        via="_rohitguptab"
       >
         <span className="icon">
           <i className="fab fa-twitter"></i>
@@ -46,7 +45,6 @@ const Share = ({ socialConfig }) => {
 
 Share.propTypes = {
   socialConfig: PropTypes.shape({
-    twitterHandle: PropTypes.string.isRequired,
     config: PropTypes.shape({
       url: PropTypes.string.isRequired,
       title: PropTypes.string.isRequired
