936754d7fc4b3fb98af05b002486d514c332bfb0
Ben Liu <ben196888@gmail.com>
2022-08-06 23:42:02 +0700

    fix(homepage-v2): nav bar should be responsive in mobile

--- src/layouts/header.jsx
+++ src/layouts/header.jsx
@@ -1,29 +1,29 @@
 const Header = ({ data }) => (
-  <header className="site-header">
+  <header className="site-header fixed-top">
     <div className="container">
-      <div className="header-main">
-        <div className="logo">
+      <nav className="navbar navbar-expand-lg">
+        <div className="logo navbar-brand">
           <a href="/">
-            <img src={data.logo} alt="logo" />
+            <img src={data.logo} height="48" alt="logo" />
           </a>
         </div>
-        <div className="responsive-menu">
-          <span></span>
-        </div>
-        <div className="menu">
-          <ul>
-            <li key="page-top">
-              <a href={`/#page-top`}>回到頁首</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarItems" aria-controls="navbarItems" aria-expanded="false" aria-label="Toggle navigation">
+          <i className="fas fa-bars"></i>
+        </button>
+        <div className="collapse navbar-collapse justify-content-end align-items-end" id="navbarItems">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link" href={`#page-top`}>{`回到頁首`}</a>
             </li>
-            <li key="project-intro">
-              <a href="/#project-intro">專案介紹</a>
+            <li className="nav-item">
+              <a className="nav-link" href={`#project-intro`}>{`專案介紹`}</a>
             </li>
-            <li key="game-intro">
-              <a href={`/#game-intro`}>遊戲介紹</a>
+            <li className="nav-item">
+              <a className="nav-link" href={`#game-intro`}>{`遊戲介紹`}</a>
             </li>
           </ul>
         </div>
-      </div>
+      </nav>
     </div>
   </header>
 )
