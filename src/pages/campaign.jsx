5225edacaeb07398f0053d5a0835a2bf2939ce6e
Ben Liu <ben196888@gmail.com>
2022-09-13 07:21:57 +0700

    refactor(campaignPage): apply new components

--- src/pages/campaign.jsx
+++ src/pages/campaign.jsx
@@ -6,7 +6,7 @@ import TwoColumns from '../components/twoColumns';
 const siteData = {
   siteName: `開源星手村`,
   description: ``,
-  logo: `images/logo.png`,
+  logo: `/images/logo.png`,
 }
 
 const socialData = {
@@ -18,7 +18,7 @@ const socialData = {
 const bannerData = {
   title: `「開源星手村」工作坊`,
   subtitle: `用桌遊，參與台灣的未來`,
-  heroImage: `images/heroimage.jpg`,
+  heroImage: `/images/heroimage.jpg`,
 }
 
 const sectionOneData = {
@@ -69,33 +69,30 @@ const sectionThreeData = {
   `,
 }
 
-const twoColumnsData = {
-  title: ``,
-  columns: [
-    [
-      `教育/文化/社會職人場`,
-      `
-      <h3>教育輔助</h3>
-      <p>開源星手村模擬了各式的專案及職業取向，在「科技資訊與媒體素養」、「人際關係與團隊合作」、「多元文化與國際理解」等課綱方向，提供寓教於樂的補充，是最強的教學輔助。</p>
-      <h3>多元互動</h3>
-      <p>人文與科技，或許並非孤立於兩端的雙子星，它們之間只是缺少了深入互動的關鍵。在開源專案裡面，需要彼此互動、合作及協商，讓我們一起透過遊戲找出關鍵，成為多方的橋樑。</p>
-      <h3>思想激盪</h3>
-      <p>在開源星手村中，由於我們彼此將扮演多種角色，可能是行銷公關，也可能是研發工程師，多方腦力激盪，能在一次次合作裡完成專案，也能讓我們在裡頭找到最適切的位置與方向。</p>
-      `
-    ],
-    [
-      `熱血青年培力探索場`,
-      `
-      <h3>拓展人脈</h3>
-      <p>透過桌遊的遊玩歷程，「關係」不斷地被建立起來，在一次次的合作與競爭中，人脈也能夠逐漸地累積。從中學習彼此的優點以及專業，是未來不可或缺的技能與經驗，加入我們吧！</p>
-      <h3>多元合作</h3>
-      <p>現今的社會已經從傳統的單打獨鬥成為群體合作，然而在群體中的個人「主體性」仍是重要的。期許在一次次的遊玩經驗中，讓彼此像映照對方的鏡子，找尋最適切的合作模式與方法。</p>
-      <h3>累積實力</h3>
-      <p>開源星手村是一個專案體驗桌遊，過程中你將累積許多對專案的了解，以及自身技能的特性。在完成專案後，我們除了能得到成就感外，還可以培養協同合作的實力，一起來變強吧！</p>
-      `
-    ]
+const upcomingEventColumns = [
+  [
+    `教育/文化/社會職人場`,
+    `
+    <h3>教育輔助</h3>
+    <p>開源星手村模擬了各式的專案及職業取向，在「科技資訊與媒體素養」、「人際關係與團隊合作」、「多元文化與國際理解」等課綱方向，提供寓教於樂的補充，是最強的教學輔助。</p>
+    <h3>多元互動</h3>
+    <p>人文與科技，或許並非孤立於兩端的雙子星，它們之間只是缺少了深入互動的關鍵。在開源專案裡面，需要彼此互動、合作及協商，讓我們一起透過遊戲找出關鍵，成為多方的橋樑。</p>
+    <h3>思想激盪</h3>
+    <p>在開源星手村中，由於我們彼此將扮演多種角色，可能是行銷公關，也可能是研發工程師，多方腦力激盪，能在一次次合作裡完成專案，也能讓我們在裡頭找到最適切的位置與方向。</p>
+    `
   ],
-}
+  [
+    `熱血青年培力探索場`,
+    `
+    <h3>拓展人脈</h3>
+    <p>透過桌遊的遊玩歷程，「關係」不斷地被建立起來，在一次次的合作與競爭中，人脈也能夠逐漸地累積。從中學習彼此的優點以及專業，是未來不可或缺的技能與經驗，加入我們吧！</p>
+    <h3>多元合作</h3>
+    <p>現今的社會已經從傳統的單打獨鬥成為群體合作，然而在群體中的個人「主體性」仍是重要的。期許在一次次的遊玩經驗中，讓彼此像映照對方的鏡子，找尋最適切的合作模式與方法。</p>
+    <h3>累積實力</h3>
+    <p>開源星手村是一個專案體驗桌遊，過程中你將累積許多對專案的了解，以及自身技能的特性。在完成專案後，我們除了能得到成就感外，還可以培養協同合作的實力，一起來變強吧！</p>
+    `
+  ]
+]
 
 const navigationList = [
   { link: `/`, text: `首頁` },
@@ -104,11 +101,16 @@ const navigationList = [
 
 const Campaign = () => (
   <Base data={{ ...siteData, social: socialData, nav: navigationList }}>
-    <Banner data={{ ...bannerData, social: {} }} />
+    <Banner
+      title={bannerData.siteName}
+      subtitle={bannerData.subtitle}
+      heroImage={bannerData.heroImage}
+      social={socialData}
+    />
     <Section data={sectionOneData} />
     <Section data={sectionTwoData} />
     <Section data={sectionThreeData} />
-    <TwoColumns data={twoColumnsData} />
+    <TwoColumns id={`upcoming-events`} title={`專案介紹`} columns={upcomingEventColumns} />
   </Base>
 )
 
