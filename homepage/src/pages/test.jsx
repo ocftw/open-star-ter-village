7f76989ef10ade0581a186d29c382a47c0dec2fa
ljc1991 <ljc19911551@gmail.com>
2023-07-03 15:30:27 +0800

    use ThreeColumns component

--- homepage/src/pages/test.jsx
+++ homepage/src/pages/test.jsx
@@ -1,5 +1,17 @@
 import Image from "next/image";
+
+import Head from "next/head";
+import Base from "../layouts/base";
+import ThreeColumns from "../components/threeColumns";
+
 import { fetchCards } from "../lib/fetchCards";
+
+const navigationList = [
+  { link: `#page-top`, text: `回到頁首` },
+  { link: `#cards-intro`, text: `卡片介紹` },
+  { link: `/campaign`, text: `活動頁` },
+  { link: `/`, text: `首頁` },
+];
 
 const getSupportLanguages = () => {
   return ["en", "zh-tw"];
@@ -9,14 +21,41 @@ export const getStaticProps = async () => {
   const lang = "zh-tw";
   const cards = await fetchCards(lang);
 
+  const updatedCards = cards.map((card) => {
+    const updatedImage = card.frontMatter.image
+      ? card.frontMatter.image.replace("/homepage/public", "")
+      : "";
+
+    return {
+      ...card,
+      frontMatter: {
+        ...card.frontMatter,
+        image: updatedImage,
+      },
+    };
+  });
+
   return {
-    props: { cards },
+    props: { cards: updatedCards },
   };
 };
 
 const test = ({ cards }) => {
   return (
-    <>
+    <Base nav={navigationList}>
+      <ThreeColumns
+        id={`cards-intro`}
+        title={`卡片介紹`}
+        columns={cards.map((card) => [
+          card.frontMatter.title,
+          `
+            <img src=${card.frontMatter.image} alt=${card.frontMatter.title}>
+            <strong>${card.frontMatter.description}</strong>
+            <div>${card.content}</div>
+            `,
+        ])}
+      />
+
       {cards.map((card) => (
         <div
           key={card.frontMatter.title}
@@ -35,14 +74,13 @@ const test = ({ cards }) => {
           <div>desc: {card.frontMatter.description}</div>
           <div>draft: {card.frontMatter.draft}</div>
           <div>type: {card.frontMatter.type}</div>
-          {/* <div>content: {card.content}</div> */}
           <div>
             content: <div dangerouslySetInnerHTML={{ __html: card.content }} />
           </div>
           <br />
         </div>
       ))}
-    </>
+    </Base>
   );
 };
 
