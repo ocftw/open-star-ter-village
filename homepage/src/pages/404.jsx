668ebf7cab45ae9e547f0688320b544209ea03ab
ljc1991 <ljc19911551@gmail.com>
2023-07-25 16:22:00 +0800

    feat: provide English translation for 404 page

--- homepage/src/pages/404.jsx
+++ homepage/src/pages/404.jsx
@@ -1,9 +1,36 @@
 import Head from 'next/head';
 import Base from '../layouts/base';
 
-const navigationList = [{ link: `/`, text: `首頁` }];
+/**
+ *
+ * @type {import('next').GetStaticProps}
+ */
+export const getStaticProps = async ({ locale }) => {
+  const homepage = {
+    en: 'Home',
+    'zh-tw': '首頁',
+  };
 
-const NotFoundPage = () => (
+  const navigationList = [{ link: `/`, text: homepage[locale] }];
+
+  const desc = {
+    en: `
+      Having an issue? Find 兔摩. <a href="https://forms.gle/t9j8dbiKUohny8PZ8">Fill out the form</a> to report the problem!
+    `,
+    'zh-tw': `
+      有問題？找兔摩。<a href="https://forms.gle/t9j8dbiKUohny8PZ8">填寫表單</a>回報你找到的問題吧！
+    `,
+  };
+
+  return {
+    props: {
+      navigationList,
+      desc: desc[locale],
+    },
+  };
+};
+
+const NotFoundPage = ({ navigationList, desc }) => (
   <Base nav={navigationList}>
     <Head>
       <title>{`開源星手村 - 找不到網頁`}</title>
@@ -12,11 +39,11 @@ const NotFoundPage = () => (
     <div className="site-container not-found-page">
       <div className="container text-center">
         <h1>NOT FOUND</h1>
-        <p>
-          有問題？找兔摩。
-          <a href="https://forms.gle/t9j8dbiKUohny8PZ8">填寫表單</a>
-          回報你找到的問題吧！
-        </p>
+        <p
+          dangerouslySetInnerHTML={{
+            __html: desc,
+          }}
+        ></p>
       </div>
     </div>
   </Base>
