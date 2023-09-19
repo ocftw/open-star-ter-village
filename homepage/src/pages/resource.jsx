1bf0f66b0cdf525a543ea8f687e772a1c60b848f
ljc1991 <ljc19911551@gmail.com>
2023-09-20 05:13:53 +0800

    feat: add navigation list to resource page

--- homepage/src/pages/resource.jsx
+++ homepage/src/pages/resource.jsx
@@ -8,9 +8,42 @@ import { fetchPage } from '../lib/fetchPage';
 export const getStaticProps = async ({ locale }) => {
   const page = fetchPage(locale, 'resource');
 
+  const homepage = {
+    en: 'Home',
+    'zh-tw': '首頁',
+  };
+
+  const cardsPage = {
+    en: 'Cards',
+    'zh-tw': '卡片頁',
+  };
+
+  const activitiesPage = {
+    en: 'Activities',
+    'zh-tw': '活動頁',
+  };
+
+  // site page navigation
+  const navigationList = [
+    { link: `/cards`, text: cardsPage[locale] },
+    { link: `/activities`, text: activitiesPage[locale] },
+    { link: `/`, text: homepage[locale] },
+  ];
+
+  /*
+  // content navigation
+  const navigationList = page.data['layout_list']?.map((layout) => {
+    return {
+      text: layout?.title,
+      link: `#${layout?.title}`,
+    };
+  });
+   */
+
   return {
     props: {
       page,
+      navigationList,
     },
   };
 };
