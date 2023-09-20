c1aab8993c54403c14fec2af3dd51dc2428cd8f3
Ben Liu <ben196888@gmail.com>
2023-09-21 00:08:11 +0700

    refactor(homepage): replace footer links from cms files

--- homepage/src/layouts/footer.jsx
+++ homepage/src/layouts/footer.jsx
@@ -1,39 +1,60 @@
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import SocialMedia from '../components/socialMedia';
 import Logo from '../components/logo';
+import footerZh from '../../_footer/zh-tw/footer.json';
+import footerEn from '../../_footer/en/footer.json';
 
-const Footer = ({ siteData }) => (
-  <div className="site-footer" id="footer">
-    <div className="container footer-main">
-      <div className="flex flex-row gap">
-        {siteData.footerLinks.map((link) => (
-          <Link
-            href={link.link}
-            key={link.text}
-            target="_blank"
-            rel="noopener noreferrer"
-            locale={false}
-          >
-            {link.text}
-          </Link>
-        ))}
-      </div>
-      <span>{siteData.title}</span>
-      <SocialMedia />
-      <div className="flex flex-row flex-justify-center logos margin-2-percent">
-        <Logo
-          text="Initiator"
-          src="/images/campaignpage/logo__OCF.png"
-          dimension={{ width: 170, height: 34 }}
-        />
-        <Logo
-          text="Sponsor"
-          src="/images/campaignpage/logo__FNF.png"
-          dimension={{ width: 163, height: 45 }}
-        />
+const Footer = ({ siteData }) => {
+  const router = useRouter();
+  const locale = router.locale;
+  const footerData = locale === 'en' ? footerEn : footerZh;
+  const footerLinks = footerData?.footer?.links ?? [];
+  return (
+    <div className="site-footer" id="footer">
+      <div className="container footer-main">
+        <div className="flex flex-row gap">
+          {footerLinks.map((link) => (
+            <Link
+              href={link.url}
+              key={link.display_text}
+              target="_blank"
+              rel="noopener noreferrer"
+              locale={false}
+            >
+              {link.display_text}
+            </Link>
+          ))}
+          {footerLinks.length === 0 &&
+            siteData.footerLinks.map((link) => (
+              <Link
+                href={link.link}
+                key={link.text}
+                target="_blank"
+                rel="noopener noreferrer"
+                locale={false}
+              >
+                {link.text}
+              </Link>
+            ))}
+        </div>
+        <span>{siteData.title}</span>
+        <SocialMedia />
+        <div className="flex flex-row flex-justify-center logos margin-2-percent">
+          <Logo
+            text="Initiator"
+            src="/images/campaignpage/logo__OCF.png"
+            dimension={{ width: 170, height: 34 }}
+          />
+          <Logo
+            text="Sponsor"
+            src="/images/campaignpage/logo__FNF.png"
+            dimension={{ width: 163, height: 45 }}
+          />
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Footer;
