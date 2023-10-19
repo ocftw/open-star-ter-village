ce5cdeaf47b680b746da9db44d9891bf28b450f4
Ben Liu <ben196888@gmail.com>
2023-10-19 12:55:16 +0700

    refactor(homepage): replace img tag w/ next/Image in cards

--- homepage/src/components/cards/card.jsx
+++ homepage/src/components/cards/card.jsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import { ParseMarkdownAndHtml } from '../parseMarkdownAndHtml';
 
 const Card = ({ card }) => (
@@ -5,7 +6,9 @@ const Card = ({ card }) => (
     <div className="section-main">
       <h3>{card.data.title}</h3>
       <div>
-        <img src={card.data.image} alt={card.data.title} />
+        <div className="image-container">
+          <Image src={card.data.image} alt={card.data.title} fill className='image' />
+        </div>
         <strong>{card.data.description}</strong>
         <ParseMarkdownAndHtml markdown={true}>
           {card.content}
