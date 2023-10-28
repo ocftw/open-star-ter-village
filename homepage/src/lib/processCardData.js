d1cb6e0b1f84d1b82df92bebd8fe7a295bade5b8
ljc1991 <ljc19911551@gmail.com>
2023-10-28 16:58:47 +0800

    refactor: move color mapping logic from client side to server side

--- homepage/src/lib/processCardData.js
+++ homepage/src/lib/processCardData.js
@@ -1,4 +1,4 @@
-import { titleToAnchorId } from "./titleToAnchorId";
+import { titleToAnchorId } from './titleToAnchorId';
 
 export function processCardData(data) {
   let image = data.image;
@@ -8,9 +8,71 @@ export function processCardData(data) {
   // Workaround for image prefix path. Will be removed after image path is fixed in all cards.
   image = !image.startsWith('/') ? `/${image}` : image;
 
+  // Non-project card
+  if (data.type === 'job') {
+    const color = {};
+
+    switch (data.tags[0]) {
+      case 'civil servants':
+        color.avatar = '#b1bb95';
+        color.background = '#8b9f6d';
+        color.border = '#6f8d50';
+        break;
+      case 'engineer':
+        color.avatar = '#95a2b2';
+        color.background = '#5e7d93';
+        color.border = '#2a6780';
+        break;
+      case 'writer':
+        color.avatar = '#94a8a1';
+        color.background = '#62867f';
+        color.border = '#287168';
+        break;
+      case 'legal':
+        color.avatar = '#d4ab8d';
+        color.background = '#c2865f';
+        color.border = '#b76a46';
+        break;
+      case 'designer':
+        color.avatar = '#d1bb8f';
+        color.background = '#be9f5f';
+        color.border = '#b18b42';
+        break;
+      case 'marketing':
+        color.avatar = '#c1907b';
+        color.background = '#aa644f';
+        color.border = '#984334';
+        break;
+      case 'advocator':
+        color.avatar = '#9a91ab';
+        color.background = '#6c688b';
+        color.border = '#474c79';
+        break;
+    }
+
+    data = {
+      ...data,
+      color,
+    };
+  }
+
+  if (data.type === 'event') {
+    const color = {};
+
+    color.avatar = '#f1c287';
+    color.background = '#eaa652';
+    color.border = ' #e6912b';
+
+    data = {
+      ...data,
+      color,
+    };
+  }
+  // project card
+
   return {
     ...data,
     image,
     id: data.id || titleToAnchorId(data.title),
-  }
+  };
 }
