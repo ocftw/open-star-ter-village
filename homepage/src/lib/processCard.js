513f188cc4573e7e645b725c7ed7ec48a37c130a
Ben Liu <ben196888@gmail.com>
2023-10-31 01:14:44 +0700

    refactor(homepage): remove unused image logic

--- homepage/src/lib/processCard.js
+++ homepage/src/lib/processCard.js
@@ -6,10 +6,7 @@ export const processCard = (card, cards) => {
 
   let image = data.image;
   const defaultImage = '/images/uploads/初階專案卡封面-01.png';
-  image = image ? image.replace('/homepage/public', '') : defaultImage;
-
-  // Workaround for image prefix path. Will be removed after image path is fixed in all cards.
-  image = !image.startsWith('/') ? `/${image}` : image;
+  image = image ?? defaultImage;
 
   data = {
     ...data,
