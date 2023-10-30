e523d45adb098e78c208139a6d40741ae9aaeb82
ljc1991 <ljc19911551@gmail.com>
2023-10-30 13:10:29 +0800

    refactor: add projectCard component

--- homepage/src/components/cards/card.jsx
+++ homepage/src/components/cards/card.jsx
@@ -1,48 +1,11 @@
-import Image from 'next/image';
 import DefaultCard from './defaultCard';
-import { ParseMarkdownAndHtml } from '../parseMarkdownAndHtml';
+import ProjectCard from './projectCard';
 
 const Card = ({ card }) => {
-  if (card.data.type !== 'project') {
-    return <DefaultCard card={card} />;
-  } else {
-    return (
-      <div className="col-md-4 mb-3" id={card.data.id}>
-        <div
-          className="section-main"
-          style={{ border: `1rem solid ${card.data.color.background}` }}
-        >
-          <h3>{card.data.title}</h3>
-          <div>
-            <div className="d-flex flex-wrap">
-              {card.data.avatarList.map((avatar) => (
-                <div
-                  key={avatar.data.title}
-                  className="col-3 avatar avatar-list"
-                  style={{
-                    backgroundColor: `${avatar.data.color.background}`,
-                    border: `3px solid ${avatar.data.color.border}`,
-                  }}
-                >
-                  <div
-                    style={{
-                      backgroundImage: `url(${avatar.data.image})`,
-                    }}
-                  >
-                    &nbsp;
-                  </div>
-                </div>
-              ))}
-            </div>
-            <strong>{card.data.description}</strong>
-            <ParseMarkdownAndHtml markdown={true}>
-              {card.content}
-            </ParseMarkdownAndHtml>
-          </div>
-        </div>
-      </div>
-    );
+  if (card.data.type === 'project') {
+    return <ProjectCard card={card} />;
   }
+  return <DefaultCard card={card} />;
 };
 
 export default Card;
