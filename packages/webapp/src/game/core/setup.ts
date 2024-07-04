77426effb98cdaa5b803de83edbc175b23d757e6
Ben Liu <ben.liu@agoda.com>
2024-07-04 10:07:56 +0700

    feat(webapp): add logs in setup

--- packages/webapp/src/game/core/setup.ts
+++ packages/webapp/src/game/core/setup.ts
@@ -17,25 +17,42 @@ type SetupFn<G extends any = any,
   ) => G;
 
 export const setup: SetupFn<GameState> = ({ ctx, random }) => {
+  console.log('setup game')
+
   const shuffler = random.Shuffle;
 
-  console.log('setup setup setup')
-
+  console.log('init state')
   // get default game state
   const G = GameStore.initialState();
 
+  // TODO: initialize rules
+
+  console.log('setup decks')
+  // add cards to decks
   DeckMutator.initialize(G.decks.projects, projectCards as unknown as ProjectCard[]);
   DeckMutator.initialize(G.decks.jobs, jobCards);
   DeckMutator.initialize(G.decks.events, eventCards);
 
+  console.log('shuffle decks')
+  // shuffle decks
+  DeckMutator.shuffleDrawPile(G.decks.projects, shuffler);
+  DeckMutator.shuffleDrawPile(G.decks.events, shuffler);
+  DeckMutator.shuffleDrawPile(G.decks.jobs, shuffler);
+
+  console.log('setup table')
+  // setup job slots
+  const maxJobCards = 8;
+  const jobCardsInPlay = DeckSelector.peek(G.decks.jobs, maxJobCards);
+  DeckMutator.draw(G.decks.jobs, maxJobCards);
+  JobSlotsMutator.addJobCards(G.table.jobSlots, jobCardsInPlay);
+
+  console.log('setup players')
+  // initialize players and score board
   PlayersMutator.initialize(G.players, ctx.playOrder);
   ScoreBoardMutator.initialize(G.table.scoreBoard, ctx.playOrder);
 
-  // shuffle cards
-  DeckMutator.shuffleDrawPile(G.decks.events, shuffler);
-
-  DeckMutator.shuffleDrawPile(G.decks.projects, shuffler);
-
+  console.log('setup player hands')
+  // setup player hands
   const maxProjectCards = 2;
   ctx.playOrder.forEach(playerId => {
     const projectCards = DeckSelector.peek(G.decks.projects, maxProjectCards);
@@ -43,12 +60,8 @@ export const setup: SetupFn<GameState> = ({ ctx, random }) => {
     PlayersMutator.addProjects(G.players, playerId, projectCards);
   });
 
-  DeckMutator.shuffleDrawPile(G.decks.jobs, shuffler);
-  const maxJobCards = 6;
-  const jobCardsInPlay = DeckSelector.peek(G.decks.jobs, maxJobCards);
-  DeckMutator.draw(G.decks.jobs, maxJobCards);
-  JobSlotsMutator.addJobCards(G.table.jobSlots, jobCardsInPlay);
-
+  console.log('setup player tokens')
+  // setup player tokens
   const numWorkerTokens = 12;
   const numActionTokens = 4;
   ctx.playOrder.forEach(playerId => {
@@ -56,5 +69,6 @@ export const setup: SetupFn<GameState> = ({ ctx, random }) => {
     PlayersMutator.resetActionTokens(G.players, playerId, numActionTokens);
   });
 
+  console.log('end setup game')
   return G;
 };
