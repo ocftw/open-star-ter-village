556005890448adc08faec210165aa9fda4e50067
Ben Liu <ben.liu@agoda.com>
2024-07-20 00:56:42 +0700

    feat(webapp): add job board component to render job cards

---
+++ packages/webapp/src/components/Table/JobBoard/JobCard.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
+import Avatar from '@mui/material/Avatar';
+import { styled } from '@mui/material/styles';
+import { useAppDispatch, useAppSelector } from '@/lib/hooks';
+import { getSelectedJobSlots, toggleJobSlotSelection } from '@/lib/reducers/jobSlotSlice';
+
+type JobCardProps = {
+  id: string;
+  title: string;
+};
+
+const StyledPaper = styled(Paper)(({ theme }) => ({
+  border: '1px solid black',
+  cursor: 'pointer',
+  padding: '16px',
+  position: 'relative',
+  display: 'flex',
+  alignItems: 'center',
+  '&:hover': {
+    backgroundColor: theme.palette.action.hover,
+  },
+  '&.selected': {
+    backgroundColor: theme.palette.action.selected,
+  },
+}));
+
+const JobCard: React.FC<JobCardProps> = ({ id, title }) => {
+  const dispatch = useAppDispatch();
+  const selectedCards = useAppSelector(getSelectedJobSlots);
+  const selected = !!selectedCards[id];
+
+  const handleSelect = () => {
+    dispatch(toggleJobSlotSelection(id));
+  };
+
+  return (
+    <StyledPaper onClick={handleSelect} className={selected ? 'selected' : ''}>
+      <Avatar alt={title} style={{ marginRight: '16px' }}>{title.slice(0,2)}</Avatar>
+      <Typography variant="h6">{title}</Typography>
+    </StyledPaper>
+  );
+};
+
+export default JobCard;
