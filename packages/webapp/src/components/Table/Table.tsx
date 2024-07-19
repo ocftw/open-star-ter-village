a2b1b95c01025d7af160d3a583916ba08ce70f16
Ben Liu <ben.liu@agoda.com>
2024-07-20 00:57:54 +0700

    refactor(webapp): move project board into table and align the design w/ job slots

--- packages/webapp/src/components/Table/Table.tsx
+++ packages/webapp/src/components/Table/Table.tsx
@@ -1,6 +1,6 @@
 import { Box, Grid } from '@mui/material';
-import ProjectSlot from '@/components/ProjectBoard/ProjectSlot';
 import { TableState } from '@/game';
+import ProjectBoard from './ProjectBoard/ProjectBoard';
 import JobSlots from './JobBoard/JobSlots';
 
 interface Props {
@@ -11,11 +11,7 @@ const Table: React.FC<Props> = (props) => {
   return (
     <Box>
       <Grid container spacing={2}>
-        {props.table.projectBoard.map((p, pIndex) => (
-          <Grid item xs={12} sm={6} md={4} key={`active-project-${pIndex}`}>
-            <ProjectSlot slot={p} />
-          </Grid>
-        ))}
+        <ProjectBoard slots={props.table.projectBoard} />
         <JobSlots jobs={props.table.jobSlots} />
       </Grid>
     </Box>
