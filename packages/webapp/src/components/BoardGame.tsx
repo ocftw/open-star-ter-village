a245d58088a4f7d643c58c164df6329ce64f0bfa
Ben Liu <ben.liu@agoda.com>
2024-07-20 00:26:22 +0700

    feat(webapp): add new game header to summary players state

--- packages/webapp/src/components/BoardGame.tsx
+++ packages/webapp/src/components/BoardGame.tsx
@@ -2,23 +2,29 @@ import { Client, BoardProps } from 'boardgame.io/react';
 import { SocketIO, Local } from 'boardgame.io/multiplayer'
 import game, { GameState } from '@/game';
 import Table from '@/components/Table/Table';
-import Players from '@/components/Players/Players';
 import DevActions from '@/components/DevActions/DevActions';
 import ActionBoard from './ActionBoard/ActionBoard';
+import GameHeader from './GameHeader/GameHeader';
 
 const Board: React.FC<BoardProps<GameState>> = (props) => {
-  const { G, ctx, debug } = props;
+  const { G, debug } = props;
   return (
     <div className='Board'>
+      <GameHeader players={G.players} scoreBoard={G.table.scoreBoard}  />
       <ActionBoard />
       <Table table={G.table} />
-      <Players players={G.players} />
       {debug && <DevActions {...props} />}
     </div>
   );
 }
 
-const Boardgame: React.FC<{ isLocal: boolean} & React.ComponentProps<ReturnType<typeof Client>>> = ({ isLocal, ...props }) => {
+type OwnProps = {
+  isLocal: boolean;
+}
+
+type Props = OwnProps & React.ComponentProps<ReturnType<typeof Client>>;
+
+const Boardgame: React.FC<Props> = ({ isLocal, ...props }) => {
   const multiplayer = isLocal ? Local() : SocketIO({ server: 'localhost:8000' });
 
   const BoardgameComponent = Client({
