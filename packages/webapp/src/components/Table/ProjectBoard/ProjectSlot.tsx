7919c2ba7d0672687d6e836e8d913d7f99dd0d0b
Ben Liu <ben.liu@agoda.com>
2024-07-23 00:31:10 +0700

    style(webapp): polish job contribution styles

--- packages/webapp/src/components/Table/ProjectBoard/ProjectSlot.tsx
+++ packages/webapp/src/components/Table/ProjectBoard/ProjectSlot.tsx
@@ -1,19 +1,23 @@
+import React from 'react';
 import { ProjectSlotState } from '@/game';
+import Avatar from '@mui/material/Avatar';
+import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
 import Chip from '@mui/material/Chip';
 import { styled } from '@mui/material/styles';
 import { playerNameMap } from '../../playerNameMap';
+import { JobAndContributions } from './JobAndContributions';
 import { useAppDispatch, useAppSelector } from '@/lib/hooks';
 import { getSelectedProjectSlots, toggleProjectSlotSelection } from '@/lib/reducers/projectSlotSlice';
-import { AvatarWithBadge } from '../../common/AvatarWithBadge';
 
 type Props = {
   slot: ProjectSlotState;
 };
 
 const StyledPaper = styled(Paper)(({ theme }) => ({
+  minHeight: '200px',
   border: '1px solid black',
   cursor: 'pointer',
   padding: '16px',
@@ -27,12 +31,12 @@ const StyledPaper = styled(Paper)(({ theme }) => ({
 }));
 
 const ProjectSlot: React.FC<Props> = ({ slot }) => {
-  const dispath = useAppDispatch();
+  const dispatch = useAppDispatch();
   const selectedSlots = useAppSelector(getSelectedProjectSlots);
   const selected = !!selectedSlots[slot.id];
 
   const handleSelect = () => {
-    dispath(toggleProjectSlotSelection(slot.id));
+    dispatch(toggleProjectSlotSelection(slot.id));
   };
 
   const projectName = slot.card?.name;
@@ -42,41 +46,24 @@ const ProjectSlot: React.FC<Props> = ({ slot }) => {
   const requiredJobs = Object.keys(slot.card?.requirements || []);
 
   return (
-    <Grid container spacing={3}>
-      <Grid item xs={12}>
-        <StyledPaper
-          onClick={handleSelect}
-          className={selected ? 'selected' : ''}
-        >
-          <Grid container direction="column" alignItems="center" justifyContent="center" style={{ minHeight: '200px' }}>
-            {!!projectType && (
-              <Chip label={projectType} color="primary" size='small' style={{ position: 'absolute', top: '16px', right: '16px' }} />
-            )}
-            <Typography variant="h6" style={{ position: 'absolute', top: '16px', left: '16px' }}>
-              {projectName}
-            </Typography>
-            <Grid container justifyContent="center" style={{ marginTop: '40px' }}>
-              {requiredJobs.map((jobName) => (
-                <Grid item key={`${slot.id}-${jobName}`} style={{ textAlign: 'center', margin: '8px' }}>
-                  <AvatarWithBadge avatarSize='large' avatarTitle={jobName} badgeContent={requirements[jobName]} />
-                  {slot.contributions
-                    .filter((contribution) => contribution.jobName === jobName)
-                    .map((contribution) => (
-                      <Typography key={`${jobName}-${contribution.worker}`}>
-                        {contribution.value}: {playerNameMap[contribution.worker]}
-                      </Typography>
-                    ))}
-                </Grid>
-              ))}
-            </Grid>
-            <Typography variant="body2" style={{ position: 'absolute', bottom: '16px', right: '16px' }}>
-              Owner: {owner}
-            </Typography>
-          </Grid>
-        </StyledPaper>
-      </Grid>
+    <Grid spacing={3} xs={12}>
+      <StyledPaper
+        onClick={handleSelect}
+        className={selected ? 'selected' : ''}
+      >
+        <Box display="flex" alignItems="center">
+          <Avatar sizes="medium">{owner}</Avatar>
+          <Typography variant="h6" sx={{ marginLeft: '8px' }}>{projectName}</Typography>
+        </Box>
+        {!!projectType && (<Chip label={projectType} color="primary" size="small" />)}
+        <Grid container direction="column" justifyContent="center" alignItems="flex-start" width='100%'>
+          {requiredJobs.map((jobName) => (
+            <JobAndContributions key={`${slot.id}-${jobName}`} jobName={jobName} requirements={requirements[jobName]} contributions={slot.contributions} />
+          ))}
+        </Grid>
+      </StyledPaper>
     </Grid>
   );
-}
+};
 
 export default ProjectSlot;
